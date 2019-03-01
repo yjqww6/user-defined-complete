b1437e8fb8b8a1cf4d98977de705a05af5a1ad20
qww6 <343519265@qq.com>
2019-03-01 21:40:23 +0800

    initial commit

---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang info
+(define collection "user-defined-complete")
+(define deps '("base" "drracket" "drracket-plugin-lib" "gui-lib"))
+(define build-deps '())
+(define pkg-desc "auto complete for user defined identifiers")
+(define version "0.1")
+(define pkg-authors '(yjqww6))
+
+
+(define drracket-tools '(("tool.rkt")))
+(define drracket-tool-names '("user-defined-complete"))
+(define drracket-tool-icons '(#f))
---
+++ private/main.rkt
@@ -0,0 +1,77 @@
+#lang racket/base
+(require racket/set syntax/kerncase)
+(provide walk)
+
+(define (walk* stxs)
+  (let loop ([ls (syntax->list stxs)])
+    (if (null? ls)
+        (seteq)
+        (set-union (walk (car ls)) (loop (cdr ls))))))
+
+(define (visible? id)
+  (for/and ([scope (in-list
+                    (hash-ref (syntax-debug-info id)
+                              'context (λ () '())))])
+    (not (eq? 'macro (vector-ref scope 1)))))
+
+(define (visible stx)
+  (syntax-case stx ()
+    [(a . b)
+     (set-union (visible #'a) (visible #'b))]
+    [x
+     (identifier? #'x)
+     (if (visible? #'x)
+         (seteq (syntax-e #'x))
+         (seteq))]
+    [_ (seteq)]))
+
+(define (walk stx)
+  (define ret
+    (kernel-syntax-case stx #f
+      [(#%expression ?expr) (walk #'?expr)]
+      [(module _ _ (#%plain-module-begin ?module-level-form ...))
+       (walk* #'(?module-level-form ...))]
+      [(begin ?expr ...)
+       (walk* #'(?expr ...))]
+      [(begin0 ?expr ...)
+       (walk* #'(?expr ...))]
+      [(begin-for-syntax ?expr ...)
+       (walk* #'(?expr ...))]
+      [(define-values (?id ...) ?expr)
+       (set-union (visible #'(?id ...)) (walk #'?expr))]
+      [(define-syntaxes (?id ...) ?expr)
+       (set-union (visible #'(?id ...)) (walk #'?expr))]
+      [(#%plain-lambda ?formals ?expr ...)
+       (set-union (visible #'?formals) (walk* #'(?expr ...)))]
+      [(case-lambda (?formals ?expr ...) ...)
+       (let loop ([formals (syntax->list #'(?formals ...))]
+                  [set (walk* #'(?expr ... ...))])
+         (cond
+           [(null? formals) set]
+           [else (loop (cdr formals)
+                       (set-union (visible (car formals))
+                                  set))]))]
+      [(if ?expr ...)
+       (walk* #'(?expr ...))]
+      [(let-values ([(?id ...) ?expr] ...)
+         ?body ...)
+       (set-union (visible #'(?id ... ...))
+                  (walk* #'(?expr ... ?body ...)))]
+      [(letrec-values ([(?id ...) ?expr] ...)
+         ?body ...)
+       (set-union (visible #'(?id ... ...))
+                  (walk* #'(?expr ... ?body ...)))]
+      [(set! ?id ?expr)
+       (walk #'?expr)]
+      [(with-continuation-mark ?expr ...)
+       (walk* #'(?expr ...))]
+      [(#%plain-app ?expr ...)
+       (walk* #'(?expr ...))]
+      [_ (seteq)]))
+  
+  (cond
+    [(syntax-property stx 'disappeared-binding)
+     =>
+     (λ (ls)
+       (set-union (visible (datum->syntax #'k ls)) ret))]
+    [else ret]))
