bccc2098cdf839b1bf4cef3b7fd0954afe1d3614
qww6 <343519265@qq.com>
2019-03-04 23:58:33 +0800

    fix phase

--- private/main.rkt
+++ private/main.rkt
@@ -2,11 +2,11 @@
 (require racket/set syntax/kerncase)
 (provide walk)
 
-(define (walk* stxs)
+(define (walk* stxs phase)
   (let loop ([ls (syntax->list stxs)])
     (if (null? ls)
         (seteq)
-        (set-union (walk (car ls)) (loop (cdr ls))))))
+        (set-union (walk (car ls) phase) (loop (cdr ls))))))
 
 (define (visible? id)
   (for/and ([scope (in-list
@@ -25,7 +25,7 @@
          (seteq))]
     [_ (seteq)]))
 
-(define (walk stx)
+(define (walk stx [phase 0])
   (define ret
     (syntax-case* stx
       (#%expression module module* begin begin0 begin-for-syntax
@@ -33,49 +33,49 @@
                     #%plain-lambda case-lambda
                     if let-values letrec-values set!
                     with-continuation-mark #%plain-app)
-      (λ (a b) (free-identifier=? a b #f #f))
-      [(#%expression ?expr) (walk #'?expr)]
+      (λ (a b) (free-identifier=? a b phase 0))
+      [(#%expression ?expr) (walk #'?expr phase)]
       [(module _ _ (#%plain-module-begin ?module-level-form ...))
-       (walk* #'(?module-level-form ...))]
+       (walk* #'(?module-level-form ...) phase)]
       [(module* _ _ (#%plain-module-begin ?module-level-form ...))
-       (walk* #'(?module-level-form ...))]
+       (walk* #'(?module-level-form ...) phase)]
       [(begin ?expr ...)
-       (walk* #'(?expr ...))]
+       (walk* #'(?expr ...) phase)]
       [(begin0 ?expr ...)
-       (walk* #'(?expr ...))]
+       (walk* #'(?expr ...) phase)]
       [(begin-for-syntax ?expr ...)
-       (walk* #'(?expr ...))]
+       (walk* #'(?expr ...) (+ phase 1))]
       [(define-values (?id ...) ?expr)
-       (set-union (visible #'(?id ...)) (walk #'?expr))]
+       (set-union (visible #'(?id ...)) (walk #'?expr phase))]
       [(define-syntaxes (?id ...) ?expr)
-       (set-union (visible #'(?id ...)) (walk #'?expr))]
+       (set-union (visible #'(?id ...)) (walk #'?expr (+ phase 1)))]
       [(#%plain-lambda ?formals ?expr ...)
-       (set-union (visible #'?formals) (walk* #'(?expr ...)))]
+       (set-union (visible #'?formals) (walk* #'(?expr ...) phase))]
       [(case-lambda (?formals ?expr ...) ...)
        (let loop ([formals (syntax->list #'(?formals ...))]
-                  [set (walk* #'(?expr ... ...))])
+                  [set (walk* #'(?expr ... ...) phase)])
          (cond
            [(null? formals) set]
            [else (loop (cdr formals)
                        (set-union (visible (car formals))
                                   set))]))]
       [(if ?expr ...)
-       (walk* #'(?expr ...))]
+       (walk* #'(?expr ...) phase)]
       [(let-values ([(?id ...) ?expr] ...)
          ?body ...)
        (set-union (visible #'(?id ... ...))
-                  (walk* #'(?expr ... ?body ...)))]
+                  (walk* #'(?expr ... ?body ...) phase))]
       [(letrec-values ([(?id ...) ?expr] ...)
          ?body ...)
        (set-union (visible #'(?id ... ...))
-                  (walk* #'(?expr ... ?body ...)))]
+                  (walk* #'(?expr ... ?body ...) phase))]
       [(set! ?id ?expr)
-       (walk #'?expr)]
+       (walk #'?expr phase)]
       [(with-continuation-mark ?expr ...)
-       (walk* #'(?expr ...))]
+       (walk* #'(?expr ...) phase)]
       [(#%plain-app ?expr ...)
-       (walk* #'(?expr ...))]
-      [_ (seteq)]))
+       (walk* #'(?expr ...) phase)]
+      [_  (seteq)]))
   
   (cond
     [(syntax-property stx 'disappeared-binding)
